55166ac7b83e6014adf675709290ab5958365461
Rakib Dhali <rakibdhali343@gmail.com>
2026-01-13 22:18:20 +0600

    first commit & completed navbar part

---
+++ src/components/Landing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='w-full h-screen '>
+        <div className="masker font-founder text-9xl ml-16">
+            <div>we create</div>
+            <div>eye-opening</div>
+            <div>presentation</div>
+        </div>
+        <hr />
+        <div>
+            <h4></h4>
+            <h4></h4>
+            <div>
+                butt
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
